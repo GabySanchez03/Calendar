9997894fdde5f7d35770fbc19e3e60954513aba8
Yednk123 <93745273+Yednk123@users.noreply.github.com>
2021-12-06 12:55:06 -0600

    updated search and icon

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -8,6 +8,10 @@ import { SidebarData } from './SidebarData';
 import './Navbar.css';
 import { IconContext } from 'react-icons';
 
+
+
+
+
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
 
@@ -15,17 +19,25 @@ function Navbar() {
 
   return (
     <>
+    
       <IconContext.Provider value={{ color: '#fff' }}>
         <div className='navbar'>
           <Link to='#' className='menu-bars'>
             <FaBars onClick={showSidebar} />
-          </Link>
+          </Link> 
         </div>
+       <div className='center'>
+       <input type='text' placeholder='serch...'/>
+         <button>Search</button>
+       </div>
+<div className='profile'>
+
+  </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav-menu-items' onClick={showSidebar}>
             <li className='navbar-toggle'>
               <Link to='#' className='menu-bars'>
-                <AiOutlineClose />
+                <FaBars />
               </Link>
             </li>
             {SidebarData.map((item, index) => {
--- Client/src/components/Sidebardata.js
+++ Client/src/components/Sidebardata.js
@@ -1,29 +1,32 @@
 import React, { useState } from 'react';
-import { FaBars } from "react-icons/fa";
-import { AiOutlineClose } from "react-icons/ai";
-import { FaBeer } from 'react-icons/fa';
+import  * as AiFillFolder from "react-icons/ai";
+import * as IoIcons  from "react-icons/io";
+import * as FaIcons  from 'react-icons/fa';
 
 
 export const SidebarData = [
 
     {
-        title: 'Employee',
+        title: 'Employees',
         path: '/Employee',
-        icon: <FaBars />,
+        icon: <IoIcons.IoMdPeople/>,
         cName: 'nav-text'
       },
 
       {
         title: 'Projects',
         path: '/Projects',
-        icon: <AiOutlineClose/>,
+        icon: <AiFillFolder.AiFillAlert/>,
         cName: 'nav-text'
       },
 
       {
         title: 'Skills',
         path: '/Skills',
-        icon: <FaBeer />,
+        icon: <FaIcons.FaPeopleArrows/>,
         cName: 'nav-text'
       },
+      
+      
+
 ];
